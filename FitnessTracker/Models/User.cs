52d156c86b08b453738b9efeb1c6f34fd061bf94
ahmad alolabi <ahmad.alolabi@learnet.se>
2024-10-28 01:26:32 +0100

    Add project files.

---
+++ FitnessTracker/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessTracker.Models
+{
+    internal class User 
+    {
+        // Properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string FitnessGoal { get; set; } // e.g., "Lose weight", "Build muscle"
+
+        public User(string name, int age, string fitnessGoal)
+        {
+            Name = name;
+            Age = age;
+            FitnessGoal = fitnessGoal;
+        }
+
+        
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"User: {Name}, Age: {Age}, Goal: {FitnessGoal}");
+        }
+    }
+}
---
+++ FitnessTracker/Models/Workout.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessTracker.Models
+{
+    internal class Workout
+    {
+        public string Type { get; set; }
+        public int Duration { get; set; } // Duration in minutes
+        public int CaloriesBurned { get; set; }
+
+        public Workout(string type, int duration, int caloriesBurned)
+        {
+            Type = type;
+            Duration = duration;
+            CaloriesBurned = caloriesBurned;
+        }
+    }
+}
+}
